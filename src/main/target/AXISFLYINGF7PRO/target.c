f92a7882f989fde3c7961807aa81f7c2276b16c6
Talha Enes Orhan <81052508+TEOTR1@users.noreply.github.com>
2026-03-19 14:57:11 +0300

    Update target.c

--- src/main/target/AXISFLYINGF7PRO/target.c
+++ src/main/target/AXISFLYINGF7PRO/target.c
@@ -23,22 +23,33 @@
 #include "drivers/timer.h"
 
 timerHardware_t timerHardware[] = {
-    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S1
-    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S2
-    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_OUTPUT_AUTO, 0, 1),  // S3
-    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S4
+    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S1 (Normal Kullanım)
+    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S2 (Normal Kullanım)
+    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_OUTPUT_AUTO, 0, 1),  // S3 (Normal Kullanım)
+    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S4 (Normal Kullanım)
 
     DEF_TIM(TIM1, CH1, PA8,  TIM_USE_SERVO, 0, 0),        // S5 (Servo)
 
-    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S6
-    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S7
-    DEF_TIM(TIM2, CH3, PB10, TIM_USE_OUTPUT_AUTO, 0, 0),  // S8
-    DEF_TIM(TIM2, CH4, PB11, TIM_USE_OUTPUT_AUTO, 0, 1),  // S9
+    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S6 (Normal Kullanım)
+    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_OUTPUT_AUTO, 0, 0),  // S7 (Normal Kullanım)
 
-    // --- DC MOTOR SINYALLERI (T2, R2, CC) ---
-    DEF_TIM(TIM5, CH3, PA2,  TIM_USE_OUTPUT_AUTO, 0, 0),  // T2 Pini (Motor Sinyali)
-    DEF_TIM(TIM5, CH4, PA3,  TIM_USE_OUTPUT_AUTO, 0, 0),  // R2 Pini (Motor Sinyali)
+    // --- T3 VE R3 PİNLERİ (SERVO KULLANIMI) ---
+    // S8 ve S9 pinleri genellikle UART3 yani T3/R3 pinleridir.
+    DEF_TIM(TIM2, CH3, PB10, TIM_USE_SERVO, 0, 0),        // T3 Pini / S8 (Servo)
+    DEF_TIM(TIM2, CH4, PB11, TIM_USE_SERVO, 0, 1),        // R3 Pini / S9 (Servo)
+
+    // --- LED STRIP PİNİ (SERVO KULLANIMI) ---
+    // NOT: Axisflying F7 Pro LED MCU pinine göre PA0 kısmını güncelleyin (PA0, PA15 veya PB3 olabilir).
+    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_SERVO, 0, 0),        // LED STRIP Pini (Servo)
+
+    // --- DİĞER MOTOR/SİNYAL PİNLERİ ---
     DEF_TIM(TIM12, CH2, PB15, TIM_USE_OUTPUT_AUTO, 0, 0), // CC Pini (Motor Sinyali)
+
+    // --- T2 VE R2 PİNLERİ (ELRS / UART KULLANIMI İÇİN KALDIRILDI) ---
+    // T2 (PA2) ve R2 (PA3) pinleri ELRS alıcısı (UART2) olarak kullanılacağı için 
+    // timerHardware dizisinden çıkarılmalıdır. Aksi takdirde timer çakışması yaşanır.
+    // DEF_TIM(TIM5, CH3, PA2,  TIM_USE_OUTPUT_AUTO, 0, 0),  // T2 Pini (İptal - ELRS TX2)
+    // DEF_TIM(TIM5, CH4, PA3,  TIM_USE_OUTPUT_AUTO, 0, 0),  // R2 Pini (İptal - ELRS RX2)
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
